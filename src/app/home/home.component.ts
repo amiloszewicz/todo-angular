b890f21c5a6f14a05b5ed6748373f74431ee2087
amiloszewicz <miloszewicz.adam@gmail.com>
2023-11-15 16:13:16 +0100

    feat element todo-list component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { TodoService } from '../shared/data-access/todo.service';
 import { TodoFormComponent } from "./ui/todo-form/todo-form.component";
+import { TodoListComponent } from "./ui/todo-list/todo-list.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     template: `
     <h2>Todo</h2>
-    <app-todo-form (todoSubmitted)="this.todoService.addTodo($event)"/>
+    <app-todo-form (todoSubmitted)="todoService.addTodo($event)"/>
+    <app-todo-list [todos]="todoService.todos()"/>
   `,
     styles: ``,
-    imports: [TodoFormComponent]
+    imports: [TodoFormComponent, TodoListComponent]
 })
 export default class HomeComponent {
   todoService = inject(TodoService);
---
+++ src/app/home/ui/todo-list/todo-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { Todo } from '../../../shared/interfaces/todo';
+
+@Component({
+  selector: 'app-todo-list',
+  standalone: true,
+  template: `
+    <ul>
+      @for (todo of todos; track $index) {
+        <li>
+          <a>{{ todo.title }}</a>
+        </li>
+      } @empty {
+        <li>Nothing to do!</li>
+      }
+    </ul>
+  `,
+  styles: ``
+})
+export class TodoListComponent {
+  @Input({ required: true }) todos!: Todo[];
+}
