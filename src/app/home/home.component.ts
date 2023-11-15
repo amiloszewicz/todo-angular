78f431e44eec5bfbdadc470207eba79b9ab14023
amiloszewicz <miloszewicz.adam@gmail.com>
2023-11-15 15:29:08 +0100

    feat component todo-form

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
+import { TodoFormComponent } from "./ui/todo-form/todo-form.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  template: `
-    <h2>
-      Todo
-    </h2>
+    selector: 'app-home',
+    standalone: true,
+    template: `
+    <h2>Todo</h2>
+    <app-todo-form />
   `,
-  styles: ``
+    styles: ``,
+    imports: [TodoFormComponent]
 })
 export default class HomeComponent {
 
