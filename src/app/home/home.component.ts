4c8e8c12bcb1c90a1a53a7b5225211673a464565
amiloszewicz <miloszewicz.adam@gmail.com>
2023-11-15 15:11:13 +0100

    feat home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  template: `
+    <h2>
+      Todo
+    </h2>
+  `,
+  styles: ``
+})
+export default class HomeComponent {
+
+}
